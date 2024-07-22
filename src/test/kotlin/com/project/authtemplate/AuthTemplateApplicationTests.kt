e7071e29f9501ee477e0459e188cf7e7a98a2ece
lgwk42 <luisgunkim@gmail.com>
2024-07-22 21:16:04 +0900

    :tada: :: 프로젝트 생성

---
+++ src/test/kotlin/com/project/authtemplate/AuthTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.authtemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthTemplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
